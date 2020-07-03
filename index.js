8f6a0f712da29698085f63c849b3c0587b7d9572
Bhavjit Chauhan <bhavjitchauhan@gmail.com>
2020-07-02 23:34:41 -0700

    Improve writing to file
    Output file is now appended to as opposed to rewriting everytime. All programs are no longer stored in a variable. If there are errors reading the previous file, the program will no longer automatically overwrite it. Users are required to use the `overwrite1 flag to avoid loss of progress.
    
    Added 'silent' flag that supressed log file.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let startTime = new Date();
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.on('keypress', (str, key) => {
-    if (key.ctrl && key.name === 'c') handleExit();
+    if (key.ctrl && key.name === 'c' && !exiting) handleExit();
 });
 
 const argv = require('yargs')
@@ -57,6 +57,11 @@ const argv = require('yargs')
         describe: 'Run in verbose mode',
         type: 'boolean'
     })
+    .option('silent', {
+        default: false,
+        describe: 'Supress logging to file',
+        type: 'boolean'
+    })
     .check((argv) => {
         if (argv.limit > argv.max) {
             logger.warn(`Limit given (${argv.limit}) is greater tham maximum program limit (${argv.max})`);
@@ -68,14 +73,22 @@ const argv = require('yargs')
     .alias('h', 'help')
     .argv;
 
+let transport;
+if (argv.silent) {
+    transport = new winston.transports.Console({ silent: true });
+} else {
+    transport = new winston.transports.File({ filename: 'log.log' });
+}
 const logger = winston.createLogger({
-    level: argv.verbose ? 'http' : 'warn',
+    level: 'info',
     format: winston.format.simple(),
-    transports: [
-        new winston.transports.File({ filename: 'log.log' })
-    ]
+    transports: [transport]
 });
 logger.info(`Initialized logger: ${startTime}`);
+
+
+let verbose = argv.verbose;
+verbose && logger.info(`In verbose mode`);
 
 for (arg in argv) {
     logger.info(`Argument '${arg}': '${argv[arg]}'`)
@@ -86,9 +99,6 @@ const omit = (object, keys) =>
         Object.entries(object)
             .filter(([key]) => !keys.includes(key))
     )
-
-let verbose = argv.verbose;
-verbose && logger.info(`In verbose mode`);
 
 let URL = 'https://www.khanacademy.org/api/internal/scratchpads/top?';
 
@@ -108,38 +118,59 @@ for (param in params) {
 }
 URL = URL.slice(0, -1);
 
-logger.info(`Initial URL: ${URL}`)
-verbose && console.debug(`Initial URL: ${chalk.gray(URL)}`);
-
+let exiting = false;
 let max = argv.max || Infinity;
 let lastCursor = '';
-let previousPrograms;
-let programs = [];
+let numberOfPrograms = 0;
 
 var file = argv.output + '.json';
 fs.readFile(file, (error, data) => {
     if (error) {
-        logger.info('No previous programs file found.')
-        scrape()
+        logger.info('No previous programs file found.');
+        logger.info(`Initial URL: ${URL}`);
+        verbose && console.debug(`Initial URL: ${chalk.gray(URL)}`);
+        scrape();
     } else {
-        logger.info(`Found previous programs file: ${file}`)
+        logger.info(`Found previous programs file: ${file}`);
         if (argv.overwrite) {
             logger.info('Overwriting previous file');
             console.log('Overwriting previous file');
+            fs.writeFile(file, '[{}', error => {
+                if (error) {
+                    logger.error(`Error writing to file ${file}: ${error}`);
+                    console.error(chalk.red(`Error writing to file ${file}: ${error}`));
+                }
+            });
+            logger.info(`Initial URL: ${URL}`);
+            verbose && console.debug(`Initial URL: ${chalk.gray(URL)}`);
             scrape();
         } else {
             try {
                 data = JSON.parse(data);
                 lastCursor = data[data.length - 1].cursor;
-                logger.info(`Using previous stored API cursor: ${lastCursor}`);
+                if (!lastCursor) {
+                    logger.error('No previous cursor found')
+                    console.error(chalk.red('No previous cursor found'));
+                    throw error;
+                }
+                logger.info(`Using previous stored API cursor: '${lastCursor}'`);
                 process.stdout.write(`File ${file} already exists. Using stored API cursor`);
                 process.stdout.write(verbose ? `: ${chalk.gray(lastCursor)}\n` : '\n');
                 data.pop();
-                previousPrograms = data;
+                fs.writeFile(file, '[' + JSON.stringify(data).slice(1, -1), error => {
+                    if (error) {
+                        logger.error(`Error writing to file ${file}: ${error}`);
+                        console.error(chalk.red(`Error writing to file ${file}: ${error}`));
+                    }
+                });
+                logger.info(`Initial URL: ${URL}`);
+                verbose && console.debug(`Initial URL: ${chalk.gray(URL + '&cursor=' + lastCursor)}`);
                 scrape(lastCursor);
             } catch (error) {
-                logger.info(`Unable to read previous file. Starting with blank cursor`);
-                scrape();
+                logger.error(`Unable to read previous file: ${error}`);
+                console.error(chalk.red(`Unable to read previous file: ${error}`));
+                console.error('Use the `--overwrite` flag to overwrite previous file');
+                process.exit();
             }
         }
     }
@@ -153,7 +184,7 @@ if (max != Infinity) {
 } else {
     logger.info('No maximum program limit set')
     const Spinner = clui.Spinner;
-    var spinner = new Spinner('Starting scraping...', ['◜', '◠', '◝', '◞', '◡', '◟']);
+    var spinner = new Spinner('Started scraping... ');
     spinner.start();
 }
 
@@ -169,47 +200,49 @@ function scrape(cursor) {
         response.on('end', () => {
             logger.http(`Recieved all chunks: '${data}'`);
             data = JSON.parse(data);
-            if (programs.length + limit * 2 <= max) {
+            lastCursor = data.cursor;
+            if (numberOfPrograms + limit * 2 <= max) {
                 max != Infinity && logger.info('Maximum program limit not reached. Sending new request');
-                lastCursor = data.cursor;
-                setTimeout(scrape, 0, data.cursor);
+                setTimeout(scrape, 0, lastCursor);
             }
             for (scratchpad of data.scratchpads) {
                 try {
                     scratchpad.thumb = scratchpad.thumb.split('/')[4].slice(0, -4);
                 } catch (error) {
-                    logger.error(`Invalid program thumbnail: '${scratchpad.thumb}'`)
-                    console.error(`${chalk.red('Invalid program thumbnail:')}: '${chalk.gray(scratchpad.thumb)}'`);
+                    logger.http(`Invalid program thumbnail: '${scratchpad.thumb}'`)
                 }
                 try {
                     scratchpad.url = scratchpad.url.split('/')[5];
                 } catch (error) {
-                    logger.error(`Invalid program URL: '${scratchpad.URL}'`)
-                    console.error(`${chalk.red('Invalid program URL:')}: '${chalk.gray(scratchpad.URL)}'`);
+                    logger.http(`Invalid program URL: '${scratchpad.URL}'`)
                 }
                 try {
                     scratchpad.created = new Date(scratchpad.created);
                 } catch (error) {
-                    logger.error(`Invalid program creation date: '${scratchpad.created}'`)
-                    console.error(`${chalk.red('Invalid program creation date:')}: '${chalk.gray(scratchpad.created)}'`);
+                    logger.http(`Invalid program creation date: '${scratchpad.created}'`)
                 }
                 try {
                     scratchpad.authorKaid = scratchpad.authorKaid.split('_')[1];
                 } catch (error) {
-                    logger.error(`Invalid program author: '${scratchpad.authorKaid}'`)
-                    console.error(`${chalk.red('Invalid program author:')}: '${chalk.gray(scratchpad.authorKaid)}'`);
+                    logger.http(`Invalid program author: '${scratchpad.authorKaid}'`)
                 }
-
-                programs.push(omit(scratchpad, ['flaggedByUser', 'key', 'translatedTitle']));
+                if (!exiting) {
+                    fs.appendFile(file, ',' + JSON.stringify(omit(scratchpad, ['flaggedByUser', 'key', 'translatedTitle'])), error => {
+                        if (error) {
+                            logger.error(`Error appending program to file: ${error}`);
+                            console.error(chalk.red(`Error appending program to file: ${error}`));
+                        }
+                    });
+                }
+                numberOfPrograms++;
+                if (max != Infinity) {
+                    process.stdout.write(progressBar.update(numberOfPrograms, max) + '\r');
+                } else {
+                    spinner.message(`${numberOfPrograms} programs scraped... `);
+                }
             }
-            updateOutputFile();
-            if (programs.length + limit > max) {
+            if (numberOfPrograms + limit > max) {
                 handleExit();
-            }
-            if (max != Infinity) {
-                process.stdout.write(progressBar.update(programs.length, max) + '\r');
-            } else {
-                spinner.message(`${programs.length} programs scraped...`);
             }
         })
     }).on('error', error => {
@@ -217,30 +250,24 @@ function scrape(cursor) {
         console.error(chalk.red(`Error requesting API: ${error}`));
     })
 }
-function updateOutputFile(final) {
-    if (programs == '') {
-        logger.warn('Attempted to write empty programs array to file');
-    };
-    if (final && lastCursor != '') {
-        programs.push({ cursor: lastCursor })
-    }
-    if (typeof previousPrograms == 'object') {
-        allPrograms = previousPrograms.concat(programs);
-    } else {
-        allPrograms = programs;
-    }
-    fs.writeFile(file, JSON.stringify(allPrograms), function (error) {
-        if (error) {
-            logger.error(`Error writing to file: ${error}`);
-            return console.error(chalk.red(`Error writing to file\n${error}`));
-        }
-        final && process.exit();
-    });
-}
 function handleExit() {
     let time = new Date() - startTime;
-    logger.info(`Completed scraping ${programs.length + limit} programs in ${time}ms`);
-    (lastCursor && verbose) && console.log(`\rLast API cursor: ${chalk.gray(lastCursor.substr(8))}`);
-    process.stdout.write(`\rCompleted scraping ${chalk.cyan(programs.length)} programs in ${chalk.cyan(time + 'ms')}.\n`);
-    updateOutputFile(true);
+    exiting = true;
+    let cursorObject = {
+        cursor: lastCursor
+    };
+    spinner.stop();
+    process.stdout.write(chalk.gray('Safely exiting...\r'));
+    setTimeout(() => {
+        fs.appendFile(file, (lastCursor != '' ? ',' + JSON.stringify(cursorObject) : '') + ']', error => {
+            if (error) {
+                logger.error(`Error appending program to file: ${error}`);
+                console.error(chalk.red(`Error appending program to file: ${error}`));
+            }
+            logger.info(`Completed scraping ${numberOfPrograms + limit} programs in ${time}ms`);
+            (lastCursor && verbose) && console.log(`\rLast API cursor: ${chalk.gray(lastCursor)}`);
+            process.stdout.write(`\rCompleted scraping ${chalk.cyan(numberOfPrograms)} programs in ${chalk.cyan(time + 'ms')}.\n`);
+            process.exit();
+        });
+    }, 1000);
 }
